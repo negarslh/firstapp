b625a432a8d448dd08f1c86d51337f6c7f89acd1
negarslh <negarslh97@gmail.com>
2023-02-11 15:34:09 +0330

    eventHandler

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -3,11 +3,29 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css";
 
 export class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text : "",
+    }
+  }
+
+  changeHandler = e => {
+    this.setState({
+      text : e.target.value
+    })
+  }
+
   render() {
     return (
       <div className={styles.container}>
         <p>What Do You Want?</p>
-        <input placeholder='Search ...'/>
+        <div>
+          <input type="text" value={this.state.text} onChange={this.changeHandler} placeholder='Search ...'/>
+          <br />
+          <br />
+          <span className={this.state.text ? styles.active : ""}>{this.state.text}</span>
+        </div>
       </div>
     )
   }
